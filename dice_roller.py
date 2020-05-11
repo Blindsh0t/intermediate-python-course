93cf195a32d991ce2ade603659cca44e5fa48895
Han Patel <han@Hardiks-MacBook-Pro.local>
2020-05-11 14:47:27 +0530

    Addition of two dice added

--- dice_roller.py
+++ dice_roller.py
@@ -7,7 +7,12 @@ def main():
     for i in range(0, dice_roll):
         roll = r.randint(1, 6)
         dice_sum += roll
-        print(f'You rolled a {roll}')
+        if roll == 1:
+            print(f'You rolled a {roll}! Critical Fail')
+        elif roll == 6:
+            print(f'You rolled a {roll}! Critical Success!')
+        else:
+            print(f'You rolled a {roll}')
     print(f'Your total is --> {dice_sum}')
 
 
